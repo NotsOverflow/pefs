6542f6fbd92b0f1748e3942a5e876aa840bd1368
Notsgnik <labruillere@gmail.com>
2015-07-27 12:09:14 +0200

    added test, formating and colors

--- pefs.py
+++ pefs.py
@@ -1,13 +1,50 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
+
+####################[Libs]##################
+
 from __future__ import print_function
 import os, sys
 
+
+####################[Vars]##################
+
+needed_binarys = [['scapy',False], ['iptables',False], ['arpspoof',False]]
+ipforward = "/proc/sys/net/ipv4/ip_forward"
+
+
+###################[Colors]#################
+
+W = '\033[0m'  # white (normal)
+R = '\033[31m'  # red
+G = '\033[32m'  # green
+O = '\033[33m'  # orange
+B = '\033[34m'  # blue
+P = '\033[35m'  # purple
+C = '\033[36m'  # cyan
+GR = '\033[37m'  # gray
+
+##################[Functions]###############
+
+def formated_print(string='',length='37',char='.',lchar='\n'):
+    the_format = "{0:%s<%s.%s}" % (char,length,length)
+    print(B+the_format.format(string)+W,end=lchar)
+
+def check_for_linux():
+    formated_print('cheking if it is run on linux',lchar="")
+    if sys.platform == "linux" or sys.platform == "linux2":
+        print(W+"["+G+"OK"+W+"]")
+    else:
+        print(W+"["+R+"error"+W+"]")
+        sys.exit(1)
+        
+
 def check_for_root():
-	print('cheking for root...',end="")
+	formated_print('cheking if it is run as root',lchar="")
 	if not os.geteuid() == 0:
-	    sys.exit('Script must be run as root')
-	print("[OK]")
+	    print(W+"["+R+"error"+W+"]")
+	    sys.exit(1)
+	print(W+"["+G+"OK"+W+"]")
 
 def check_for_binarys(binarys_array=[]):
 	paths = os.environ["PATH"].split(':') #Tous les répertoires contenant des executables.
@@ -17,31 +54,37 @@ def check_for_binarys(binarys_array=[]):
 				needed_binary[1] = True
 
 	for needed_binary in binarys_array:
-		print('cheking for '+needed_binary[0]+"...",end="")
+		formated_print('cheking if '+needed_binary[0]+" is installed",lchar="")
 		if not needed_binary[1]:
-			sys.exit("[error]")
-		print("[OK]")
+			print(W+"["+R+"error"+W+"]")
+	        sys.exit(1)
+		print(W+"["+G+"OK"+W+"]")
 
 def check_file_exist_and_is_writeateble(file_path=""):
-	name = file_path.split('/')[-1] #getting the last element of the file_path
-	print('cheking if '+name+' exist...',end="")
-	if not os.path.isfile(file_path):
-		sys.exit("[error]")
-	print("[OK]")
-	try:
-		print('cheking if '+name+' is writeable...',end="")
-		file = open(file_path, "w")
-		file.close()
-		print("[OK]")
-		return True
-	except:
-		print("[error]")
-		return False
+    name = file_path.split('/')[-1] #getting the last element of the file_path
+    formated_print('cheking if '+name+' exist',lchar="")
+    if not os.path.isfile(file_path):
+        print(W+"["+R+"error"+W+"]")
+        sys.exit(1)
+    print(W+"["+G+"OK"+W+"]")
+    try:
+        formated_print('cheking if '+name+' is writeable',lchar="")
+        file = open(file_path, "w")
+        file.close()
+        print(W+"["+G+"OK"+W+"]")
+    except:
+        print(W+"["+R+"error"+W+"]")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
-	needed_binarys = [['scapy',False], ['iptables',False], ['arpspoof',False]]
-	check_for_root()
-	check_for_binarys(needed_binarys)
-	if not check_file_exist_and_is_writeateble("/proc/sys/net/ipv4/ip_forward"):
-		sys.exit("")
+    check_for_linux()
+    check_for_root()
+    check_for_binarys(needed_binarys)
+    check_file_exist_and_is_writeateble(ipforward)
+
+
+
+
+
+
